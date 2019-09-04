b2d88dc7d29b6599ae7572e50311052bebe5d505
Caitlin Majewski <cmajewski@wesleyan.edu>
2019-09-04 13:27:06 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -7,7 +7,7 @@ class EmailAddressParser
   end
 
   def parse
-  new_email=@email_addresses.split(/[ ,]/).reject.uniq {|email| email.empty?}
+  new_email=@email_addresses.split(/[ ,]/).uniq.reject {|email| email.empty?}
   end
 end
 
